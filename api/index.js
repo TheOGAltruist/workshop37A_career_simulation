e5713384a0e5296e28ce9c3750db3a86383b34ac
TheOGAltruist <staraknath@yahoo.co.in>
2024-12-07 20:00:35 +0000

    Added Auth, Review and Comment routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const express = require("express")
 const app = express()
 const userRouter = require("./routes/users.routes.js")
 const itemRouter = require("./routes/items.routes.js")
+const authRouter = require("./routes/auth.routes.js")
+const reviewRouter = require("./routes/review.routes.js")
+const commentrouter = require("./routes/comment.routes.js")
 
 app.use(require("morgan")("dev"))
 app.use(express.json())
@@ -11,6 +14,9 @@ app.listen(process.env.PORT, () => {console.log("App running on Port 3000..")})
 
 app.use("/api", userRouter)
 app.use("/api", itemRouter)
+app.use("/api", authRouter)
+app.use("/api", reviewRouter)
+app.use("/api", commentrouter)
 
 
 app.use((err, req, res, next) => {
