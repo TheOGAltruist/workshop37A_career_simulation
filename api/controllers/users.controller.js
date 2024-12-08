889de6418c8111f911d7df215bfc9072561d2237
TheOGAltruist <staraknath@yahoo.co.in>
2024-12-08 05:13:24 +0000

    Added changeReview and changeComment functions to the user controller

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,9 +1,6 @@
 const prisma = require("../../prisma")
 
-const me = async (req, res, next) => {
-
-}
-
+//Get all users
 const allUsers = async (req, res, next) => {
     try {
         const users = await prisma.user.findMany({
@@ -11,6 +8,11 @@ const allUsers = async (req, res, next) => {
                 reviews: true,
                 comments:true
             },
+            select: {
+                id: true,
+                name: true,
+                username: true
+            }
         })        
         res.status(200).json(users)
     } catch (error) {
@@ -18,9 +20,46 @@ const allUsers = async (req, res, next) => {
     }
 };
 
+//Modify self review (Protected)
+const changeReview = async (req, res, next) => {
+    try {
+        const response = await prisma.review.update({
+            where: {
+                user_id: req.user.id,
+                id: +req.params.reviewId
+            },
+            data: {
+                text: req.body.text
+            }
+        });
 
+        res.status(204).send()
+    } catch (error) {
+        next(error)
+    }
+}
+
+//Modify self comment (Protected)
+const changeComment = async (req, res, next) => {
+    try {
+        const response = await prisma.comment.update({
+            where: {
+                user_id: req.user.id,
+                id: +req.params.reviewId
+            },
+            data: {
+                text: req.body.text
+            }
+        });
+
+        res.status(204).send()
+    } catch (error) {
+        next(error)
+    }
+}
 
 module.exports = {
-    me,
     allUsers,
+    changeReview,
+    changeComment,
 }
