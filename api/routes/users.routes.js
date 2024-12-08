7a79c01a5acb3cdd0c3302d3b11be4a447c02678
TheOGAltruist <staraknath@yahoo.co.in>
2024-12-08 05:14:13 +0000

    Added changeReview and changeComment routes

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,8 +1,13 @@
 const { 
-    allUsers
+    allUsers,
+    changeReview,
+    changeComment,
 } = require("../controllers/users.controller.js")
 
+const { isLoggedIn } = require("../utils/middleware.util.js")
 const router = require("express").Router()
 module.exports = router
 
-router.get("/users",allUsers)
+router.get("/users", isLoggedIn, allUsers)
+router.put("/users/:userId/reviews/:reviewId", isLoggedIn, changeReview)
+router.put("/users/:userId/comments/:commentId", isLoggedIn, changeComment)
